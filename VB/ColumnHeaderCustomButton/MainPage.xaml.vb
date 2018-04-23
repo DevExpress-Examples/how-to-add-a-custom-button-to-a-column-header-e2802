a6bf651ca6a020180c2364e4a6358002f0ca9aef
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/ColumnHeaderCustomButton/MainPage.xaml.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace ColumnHeaderCustomButton
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New ProductList()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			' Process mouse clicks.
+		End Sub
+	End Class
+End Namespace
