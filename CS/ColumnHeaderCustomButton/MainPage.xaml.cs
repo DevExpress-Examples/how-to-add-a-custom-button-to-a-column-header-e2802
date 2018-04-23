e52f9bffc975e139d81dde382900a2fccd9a9b7a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ColumnHeaderCustomButton/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ColumnHeaderCustomButton {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new ProductList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            // Process mouse clicks.
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to add a custom button to a column header
+
+
+<p>This example demonstrates how to display a button within a column header's customization area.</p>
+
+<br/>
+
+
